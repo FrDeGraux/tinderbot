f05da52eb1a2dc029f8cd13b5e084288f7ba6edb
FrDeGraux <francoisdegraux@gmail.com>
2024-04-09 18:17:39 -0400

    Beta

--- TinderBot.py
+++ TinderBot.py
@@ -17,7 +17,8 @@ class TinderBot(object) :
         self.dialog = Dialog()
         self.chatgpt = ChatGPTCustom()
         self.tinder_api = TinderAPI(os.getenv('TINDER_TOKEN'))
-      #  self.reply_messages()
+        self.profile = self.tinder_api.profile()
+        self.user_id = self.profile.id
         pass
     def get_languages(self,in_match):
         sBio = in_match.person.bio
@@ -25,18 +26,18 @@ class TinderBot(object) :
         response = 'French'
      #   response = chatgpt.get_response(strRqst)
         return response.lower().replace('.', '')
-    def send_response(self,chatroom, response, content, from_user_id, to_user_id):
-        if response:
-            response = cc.convert(response)
-            if response.startswith('[Sender]'):
-                chatroom.send(response[8:], from_user_id, to_user_id)
-            else:
-                chatroom.send(response, from_user_id, to_user_id)
-            logger.info(f'Content: {content}, Reply: {response}')
+    def send_response(self, response, to_user_id):
+        chatroom = self.tinder_api.get_messages(to_user_id)
+        chatroom.send(response, self.user_id, to_user_id)
+
+        logger.info(f' Reply: {response}')
     def get_matches_names(self):
-        matches = self.tinder_api.matches(limit=10)
+        matches = self.get_matches()
         names = [{'name': item.person.name, 'id' : item.match_id} for item in matches]
         return names
+    def get_matches(self):
+        matches = self.tinder_api.matches(limit=10)
+        return matches
 
     def process_messages(self,match_id):
         chatroom = self.tinder_api.get_messages(match_id)
@@ -46,18 +47,26 @@ class TinderBot(object) :
             prefix = "Me: " if message.from_id == os.getenv('MY_TINDER_ID')  else "Her: "
             result_text += prefix + message.message+ '\n'
         return result_text.strip()  # Remove the last newline character
+    def has_new_message(self,in_match_id):
+        chatroom = self.tinder_api.get_messages(in_match_id)
+        lastest_message = chatroom.get_lastest_message()
+        if lastest_message:
+            if (lastest_message.from_id == (in_match_id)) :
+                return True
+        return False
+
     def reply_messages(self):
-        try :
+        try:
             profile = self.tinder_api.profile()
-        except (JSONDecodeError) :
-            raise Exception(" Token "  + os.getenv('TINDER_TOKEN') + " not existing anymore")
+        except (JSONDecodeError):
+            raise Exception(" Token " + os.getenv('TINDER_TOKEN') + " not existing anymore")
         user_id = profile.id
 
         for match in self.tinder_api.matches(limit=50):
             chatroom = self.tinder_api.get_messages(match.match_id)
             lastest_message = chatroom.get_lastest_message()
             pass
-            """
+
             if (match.language is None):
                 match.language = self.get_languages(match)
             chatroom = self.tinder_api.get_messages(match.match_id)
@@ -72,7 +81,7 @@ class TinderBot(object) :
                     to_user_id = lastest_message.from_id
                     last_message = 'other'
                 sent_date = lastest_message.sent_date
-                if last_message == 'other' or (sent_date + datetime.timedelta(days=1)) < datetime.datetime.now(): # Relance après un jour
+                if last_message == 'me' or (sent_date + datetime.timedelta(days=1)) < datetime.datetime.now(): # Relance après un jour
                     content = self.dialog.generate_typic_input(from_user_id, to_user_id, chatroom.messages[::-1])
                     list_responses = self.chatgpt.ask_to_gpt(content,int(os.getenv('N_ALTERNATIVES')))
                     pass
@@ -85,4 +94,34 @@ class TinderBot(object) :
                # response = self.chatgpt.ask_to_gpt(content)
                 #Telegram
               #  self.send_response(chatroom, response, content, user_id, match.match_id)
-              """
+
+    def reply_messages_v2(self,match):
+
+            if (match.language is None):
+                match.language = self.get_languages(match)
+            chatroom = self.tinder_api.get_messages(match.match_id)
+            lastest_message = chatroom.get_lastest_message()
+            if lastest_message: # is there a new message ?
+                if lastest_message.from_id == self.user_id: # last message comes from me
+                    from_user_id = lastest_message.from_id
+                    to_user_id = lastest_message.to_id
+                    last_message = 'me'
+                else:
+                    from_user_id = lastest_message.to_id # last message comes from her
+                    to_user_id = lastest_message.from_id
+                    last_message = 'other'
+                sent_date = lastest_message.sent_date
+                if last_message == 'me' or ((sent_date + datetime.timedelta(days=1)) < datetime.datetime.now()): # Relance après un jour
+                    content = self.dialog.generate_typic_input(from_user_id, to_user_id, chatroom.messages[::-1])
+                    list_responses = [str(i) for i in range(1,10)]
+                   # list_responses = self.chatgpt.ask_to_gpt(content,int(os.getenv('N_ALTERNATIVES')))
+                    return list_responses
+                    # Telegram
+                    #self.send_response(chatroom, response, content, from_user_id, to_user_id)
+
+            else: # No new message
+                return None
+               # content = self.dialog.generate_initialize_input(user_id, match.match_id, match)
+               # response = self.chatgpt.ask_to_gpt(content)
+                #Telegram
+              #  self.send_response(chatroom, response, content, user_id, match.match_id
--- telegram_nested_test_v3.py
+++ telegram_nested_test_v3.py
@@ -6,12 +6,14 @@ from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, CallbackContext
 
 load_dotenv('.env')
-
+odd_numbers = [num for num in range(3, 51, 2)]
+even_numbers = [num for num in range(2, 51, 2)]
 
 class TelegramBot:
     ITEMS_PER_PAGE = 5
     MAX_LEVELS = 5  # Maximum depth of the menu
-
+    odd_numbers = [num for num in range(3, MAX_LEVELS, 2)]
+    even_numbers = [num for num in range(2, MAX_LEVELS, 2)]
     def __init__(self):
         self.updater = Updater(os.getenv('TELEGRAM_TOKEN'))
         dp = self.updater.dispatcher
@@ -19,54 +21,67 @@ class TelegramBot:
         dp.add_handler(CallbackQueryHandler(self.button))
         dp.add_handler(MessageHandler(Filters.text & ~Filters.command, self.handle_custom_text))
         self.tbot = TinderBot()
+        self.matches = self.tbot.get_matches()
+        self.custom_text = None
         self.selected_match_id = ""
+        self.text = ""
+        self.gpt_replies = []
+        self.level = None
+        self.page = None
 
     def start(self, update: Update, context: CallbackContext) -> None:
         keyboard = self.get_menu(level=1, page=0)
         update.message.reply_text('Choose an option:', reply_markup=keyboard)
 
     def get_menu(self, level, page):
+        self.level = level
+        self.page = page
         start_idx = page * self.ITEMS_PER_PAGE
         end_idx = start_idx + self.ITEMS_PER_PAGE
 
-        if level == 1:
+        if self.level == 1:
+
             matches_dict = self.tbot.get_matches_names()
             matches_names = [item['name'] for item in matches_dict]
             matches_id = [item['id'] for item in matches_dict]
-
             items = matches_names[start_idx:end_idx]
             ids = matches_id[start_idx:end_idx]
-
+            has_new_messages = [self.tbot.has_new_message(match) for match in matches_id]
+            matches = self.matches[start_idx:end_idx]
 
             buttons = [
-                [InlineKeyboardButton(text=item, callback_data=f"{level}_{item}_{page}_{id}")] for item, id in
+                [InlineKeyboardButton(text=item, callback_data=f"{self.level}_{item}_{self.page}_{id}")] for item, id in
                 zip(items, ids)
             ]
         else :
-            if level in [2, 4, 5]:
-                items = [f"ChatGPTLabel{i}" for i in range(start_idx + 1, end_idx + 1)]
-            if level == 3:
-                items = [f"Cocky{i}" for i in range(start_idx + 1, end_idx + 1)]
+            if self.level in self.even_numbers:
+                items_shown = [self.gpt_replies[i] for i in range(start_idx + 1, end_idx + 1)]
+            if self.level in self.odd_numbers:
+                items_shown = [f"Cocky{i}" for i in range(start_idx + 1, end_idx + 1)]
 
             buttons = [
-                [InlineKeyboardButton(text=item, callback_data=f"{level}_{item}_{page}")] for item in items
+                [InlineKeyboardButton(text=item, callback_data=f"{self.level}_{item}_{self.page}")] for item in items_shown
             ]
 
-        buttons.append([InlineKeyboardButton(text="Send Your Text", callback_data=f"{level}_custom_{page}")])
+        buttons.append([InlineKeyboardButton(text="Send Your Text", callback_data=f"{self.level}_custom_{self.page}")])
 
         # Navigation buttons
         navigation_buttons = []
         if start_idx > 0:
-            navigation_buttons.append(InlineKeyboardButton(text="<< Prev", callback_data=f"{level}_prev_{page - 1}"))
-        navigation_buttons.append(InlineKeyboardButton(text="Next >>", callback_data=f"{level}_next_{page + 1}"))
+            navigation_buttons.append(InlineKeyboardButton(text="<< Prev", callback_data=f"{self.level}_prev_{self.page - 1}"))
+        navigation_buttons.append(InlineKeyboardButton(text="Next >>", callback_data=f"{self.level}_next_{self.page + 1}"))
         if navigation_buttons:
             buttons.append(navigation_buttons)
 
         return InlineKeyboardMarkup(buttons)
-
+    def from_match_id_to_match_object(self,in_sMatchID):
+        for match in self.matches:
+            if match.match_id == in_sMatchID:
+                return match
+        return None  # Return None if match_id is not found in the list
     def handle_custom_text(self, update: Update, context: CallbackContext) -> None:
-        text = update.message.text
-        update.message.reply_text(f"Received your text: {text}")
+        self.custom_text = update.message.text
+        update.message.reply_text(f"Received your text: {self.custom_text}")
 
         # Define the buttons
         yes_button = InlineKeyboardButton(text="Yes", callback_data="0_yes")
@@ -75,7 +90,6 @@ class TelegramBot:
 
         # Send the buttons as a reply
         update.message.reply_text('Do you want to proceed?', reply_markup=keyboard)
-
     def button(self, update, context) -> None:
         query = update.callback_query
         query.answer()
@@ -85,33 +99,39 @@ class TelegramBot:
             self.selected_match_id = data[3]
         action = data[1]
 
+
         if action == 'yes':
+            self.tbot.send_response(self.custom_text,self.selected_match_id)
             query.edit_message_text(text="Message Sent!")
             return
-        elif action == 'custom':
+        if action == 'custom':
             query.edit_message_text(text="Please send your custom text now.")
             return
-        elif action in ["next", "prev"]:
-            level = int(data[0])
-            page = int(data[2])
-            keyboard = self.get_menu(level, page)
-            query.edit_message_text(text=f"Level {level} - Choose an option:", reply_markup=keyboard)
-        else:
-            level = int(data[0])
-            item = data[1]
-            page = int(data[2])
-            if level < self.MAX_LEVELS:
-                keyboard = self.get_menu(level + 1, 0)  # Move to the next level
-                text = self.tbot.process_messages(self.selected_match_id)
-                text_parts = [text[i:i + 4000] for i in
-                              range(0, len(text), 4000)]  # Splitting into parts of 4000 characters each
+        else :
+            if action != "no" :
+                self.level = int(data[0])
+                self.page = int(data[2])
+            if action in ["next", "prev"]:
 
-                # Sending each part as a separate message
-                for part in text_parts.reverse():
-                    query.edit_message_text(text=part, reply_markup=keyboard)
-
+                keyboard = self.get_menu(self.level, self.page)
+                query.edit_message_text(text=f"Level {self.level} - Choose an option:", reply_markup=keyboard)
             else:
-                query.edit_message_text(text=f"You reached the final selection: {item}")
+                item = data[1]
+                if self.level < self.MAX_LEVELS:
+                    if (self.level+1)  in even_numbers :
+                        self.gpt_replies = self.tbot.reply_messages_v2(self.from_match_id_to_match_object(self.selected_match_id))
+
+                    keyboard = self.get_menu(self.level + 1, 0)  # Move to the next level
+                    self.text = self.tbot.process_messages(self.selected_match_id)
+                    text_parts = [self.text[i:i + 4000] for i in
+                                  range(0, len(self.text), 4000)]  # Splitting into parts of 4000 characters each
+                  #     text_parts = text_parts.reverse()
+                    # Sending each part as a separate message
+                    for part in text_parts:
+                        query.edit_message_text(text=part, reply_markup=keyboard)
+
+                else:
+                    query.edit_message_text(text=f"You reached the final selection: {item}")
 
     def run(self):
         self.updater.start_polling()
